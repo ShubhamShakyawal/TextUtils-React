2c601e6cb64c600c388bb1cfc7c5a6c995c881f8
ShubhamShakyawal <shakyawals@gmail.com>
2021-12-17 16:27:10 +0530

    Updates

---
+++ precache-manifest.dae77c85fdd80334b5c74f49bae3c41f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2a83ced1ed6415e73c6",
+    "url": "/TextUtils-React/static/css/main.cd377c3e.chunk.css"
+  },
+  {
+    "revision": "b2a83ced1ed6415e73c6",
+    "url": "/TextUtils-React/static/js/main.b2a83ced.chunk.js"
+  },
+  {
+    "revision": "8e088fc206c9a0b3d540",
+    "url": "/TextUtils-React/static/js/1.8e088fc2.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils-React/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "b18d17daa8e3384e0741",
+    "url": "/TextUtils-React/static/js/runtime~main.b18d17da.js"
+  },
+  {
+    "revision": "def689092506f7bd4c72983d4cee0234",
+    "url": "/TextUtils-React/index.html"
+  }
+];
